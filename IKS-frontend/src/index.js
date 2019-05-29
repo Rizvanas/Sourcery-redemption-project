d5f69be8f3a810f45ad8139d3d1e9dd15f1c8bc4
Rizvan <rizvanchalilovas@gmail.com>
2019-05-29 17:32:08 +0300

    Fix pre commit hook errors

--- IKS-frontend/src/index.js
+++ IKS-frontend/src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import reducers from "./reducers/CombinedReducers";
 
 const rootElement = document.getElementById("root");
-const store = createStore(reducers, applyMiddleware(reduxThunk));
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider>
     <App />
   </Provider>,
   rootElement

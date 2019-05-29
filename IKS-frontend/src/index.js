a2577717a346b44a0676a9a75a4efcb9a23bc534
Rizvan <rizvanchalilovas@gmail.com>
2019-05-29 17:32:58 +0300

    Fix pre commit hook errors

--- IKS-frontend/src/index.js
+++ IKS-frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reduxThunk from "redux-thunk";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
